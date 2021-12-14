9d67426822e93d1b8dbe7de87560bbcf103c8d4d
Jay Pfaffman <pfaffman@gmail.com>
2021-12-14 08:48:53 -0500

    match links in text-only email

--- plugin.rb
+++ plugin.rb
@@ -25,8 +25,9 @@ after_initialize do
     next if self.custom_fields[GOOGLE_GROUP_LINK]
     p = Post.find_by(topic_id: topic.id, post_number: 1)
     m = /To view this discussion on the web visit <a href=3D"(.*?)"/m.match(p.raw_email)
-    next unless m
-    link = m[1].gsub(/=\n/, "")
+    n = /To view this discussion on the web visit (https:\/\/groups.google.com\/.*?gmail.com)\./m.match(p.raw_email)
+    next unless m || n
+    link = (m || n)[1]
     self.custom_fields[GOOGLE_GROUP_LINK] = link
     rescue => e
       Rails.logger.warn("Google link: add_google_group_link #{topic.title} failed")
@@ -43,4 +44,11 @@ after_initialize do
       Rails.logger.warn("Google link: callback #{t.title} failed")
   end
 
+  add_to_class(:topic, :google_group_link_ensure_consistency) do
+    fix_these_topics = Topic.where(category_id: 5)
+    fix_these_topics.each do |t|
+      t.add_google_group_link
+        t.save
+    end
+  end
 end
